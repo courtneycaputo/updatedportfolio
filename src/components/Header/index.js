d0c1fc1b1fd83021de0fe667acf309ec4b4b9153
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-22 09:59:37 -0800

    update index.css with new color theme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,8 +7,12 @@ function NavTabs({ currentPage, handlePageChange }) {
   
   return (
 
-<nav className="navbar navbar-expand-lg " style={headerStyle}>
-  <div className="container-fluid" style={headerStyle}>
+<nav className="navbar navbar-expand-lg" 
+      // style={headerStyle}
+      >
+  <div className="container-fluid" 
+  // style={headerStyle}
+  >
     <div className="navTitle">Courtney Caputo</div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
